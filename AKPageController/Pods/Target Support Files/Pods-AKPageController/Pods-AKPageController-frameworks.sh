a8ac06119a916afa81edd91663c9664c7ddbf378
Little.Daddly <575385842@qq.com>
2019-09-05 10:43:43 +0800

    移除无关库

--- AKPageController/Pods/Target Support Files/Pods-AKPageController/Pods-AKPageController-frameworks.sh
+++ AKPageController/Pods/Target Support Files/Pods-AKPageController/Pods-AKPageController-frameworks.sh
@@ -162,27 +162,17 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FLAnimatedImage/FLAnimatedImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YYCache/YYCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYCategories/YYCategories.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YYText/YYText.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FLAnimatedImage/FLAnimatedImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YYCache/YYCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYCategories/YYCategories.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YYText/YYText.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- README.md
+++ README.md
@@ -3,7 +3,16 @@
 
 
 
-使用方法。将AKPageController 拖入工程
+使用方法。
 
-载体vc参考AKDemoVC；
-底部滚动参考AKCommenVC
+1.将AKPageController 拖入工程
+
+2.载体vc参考AKDemoVC；
+3.底部滚动参考AKCommenVC
+4.手动拖入BeautyKit 中的工具类 即可。  [可以移除将使用的工具类替换自己的工具类]
+5.集成pod
+pod 'YYCategories','1.0.4'
+pod 'ReactiveObjC','3.1.1'
+pod 'Masonry','1.1.0'
+pod 'SDWebImage','4.4.6'     
+pod 'SDWebImage/GIF', '4.4.6'
